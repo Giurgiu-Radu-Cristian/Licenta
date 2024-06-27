a90a18fd73b48d6868d351b9b27da2624030960d
Giurgiu-Radu-Cristian-30126 <giurgiuradu22@yahoo.com>
2024-06-27 16:53:40 +0300

    design la contract details

--- src/models/Contract.js
+++ src/models/Contract.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const contractSchema = new mongoose.Schema({
     contractId: { type: String, required: true },
     University: { type: String, required: true },
-    counterparty: { type: String, required: true },
-    counterpartyLocation: {
+    Counterparty: { type: String, required: true },
+    "Counterparty location": {
         city: { type: String, required: true },
         country: { type: String, required: true },
         lat: { type: Number, required: true },
@@ -13,11 +13,11 @@ const contractSchema = new mongoose.Schema({
     images: [String],
     counterpartyDomain: { type: String, required: true },
     contractType: { type: String, required: true },
-    purpose: { type: String, required: true },
+    Purpose: { type: String, required: true },
     objectives: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    duration: { type: String, required: true },
-    status: { type: String, required: true }
+    "Contract Start Date": { type: String, required: true },
+    Duration: { type: String, required: true },
+    Status: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Contract', contractSchema);
